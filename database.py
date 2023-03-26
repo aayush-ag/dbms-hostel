b1786ce9af550edbaafcd2a41bc51bdf2f369155
Aayush <agcdev0@gmail.com>
2023-03-26 13:05:15 +0000

    Update3

--- database.py
+++ database.py
@@ -1,10 +1,11 @@
 import mysql.connector
+import os
 
 db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="root",
-    database="db"
+    host=os.environ.get('DB_HOST'),
+    user=os.environ.get('DB_USER'),
+    password=os.environ.get('DB_PASSWORD'),
+    database=os.environ.get('DB_NAME')
 )
 
 def get_db():
